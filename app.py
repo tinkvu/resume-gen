92534e37548eb313c928814d2f58cd9040e98342
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2025-04-27 13:08:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 import json
 from fpdf import FPDF  # Keep original import, but install fpdf version 2+
 import textwrap
-from dotenv import load_dotenvfrom io import StringIO
+import StringIO
 from pdfminer.high_level import extract_text as extract_pdf_text
 import docx2txt
 
